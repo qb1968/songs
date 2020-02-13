8146c17399e676843e173b25aa2a7a44b9d09761
qb1968 <you@example.com>
2020-02-13 15:49:45 -0500

    maybe

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  const songs = [
+    { id: 1, name: "Final Countdown", secret: process.env.SECRET },
+  ];
+
+  res.status(200).json(songs);
+});
+
+// make the port dynamic
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+// npm i -D nodemon
+// npx gitignore node
---
+++ notes.md
@@ -0,0 +1,12 @@
+# Deployment Notes
+
+## Steps
+
+- create an empty repository on GitHub
+- clone it
+- open it in your code editor
+- create a default package.json by typing: `npm init -y`
+- `npm i express`
+- `git status` => `node_modules` shows up on the list.
+- `npx gitignore node` to generate a `.gitignore` super file
+  - or: `npm i -g gitignore` => `gitignore node`.
